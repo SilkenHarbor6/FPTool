46851262f18e3134099e71720aa259ab391d08cc
Bryan <bryanmauriciopalma@hotmail.com>
2023-01-11 19:15:08 -0600

    NBT Reader done

---
+++ FPTool/Form1.cs
@@ -0,0 +1,33 @@
+﻿using fNbt;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FPTool
+{
+    public partial class Form1 : Form
+    {
+        string filePath= "C:\\Users\\bryan\\AppData\\Roaming\\.minecraft\\saves\\Final_Paradox_v1.0.7\\data\\scoreboard.dat";
+        public Form1()
+        {
+            InitializeComponent();
+            test(filePath);
+        }
+        void test(string Path)
+        {
+            NbtFile file = new NbtFile(Path);
+            var root = file.RootTag;
+            var RootTags = (NbtCompound)root.Tags.ToList()[0];
+            var dataIndex = RootTags.Names.ToList().IndexOf("Objectives");
+            var data = (NbtList)RootTags.Tags.ToList()[dataIndex];
+
+        }
+        
+    }
+}
